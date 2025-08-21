d74596b45d9f646ea911e102e020ac7b9a07f6c4
moaoyama <mo.aoyama.303@gmail.com>
2025-08-22 00:58:38 +0900

    Update HTML/CSS/JS

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,11 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  # 新しいユーザー登録フォームを表示するアクション
+  def new
+    super
+  end
+
+  # 新しいユーザー登録を作成するアクション
+  def create
+    super
+  end
+end
