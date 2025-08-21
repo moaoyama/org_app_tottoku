005946f1726e3a66e85e8cf9ea5f0ec748dd8f43
moaoyama <mo.aoyama.303@gmail.com>
2025-08-22 00:39:45 +0900

    Update HTML/CSS/JS

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  # Deviseのルート（ログイン / サインアップ / ログアウト）
+  # Deviseのルート
   devise_for :users, class_name: 'User', controllers: {
-    sessions: 'users/sessions' # ここで、新しく作ったコントローラーを使うことを教える
+    sessions: 'users/sessions'
+    registrations: 'users/registrations'
   }
+
+  # ゲストログイン
+  devise_scope :user do
+    post 'guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
+  end
 
   # 管理画面
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
@@ -11,36 +18,20 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'static_pages/home'
 
-  # ユーザー関連（表示や編集だけ残す/n&cはDeviseが担当）
+  # ユーザー関連
   resources :users, only: [:show, :edit, :update]
+  get '/mypage', to: 'users#show', as: :mypage
   
   # Documents関連
-  resources :documents, only: [:create, :show, :index, :edit, :update, :destroy] do
+  resources :documents do
     member do
       get :result, as: :result_document
-      patch :update_location
-      patch :update_user_comment
-      patch :update_judgement
+      patch :update_location, :update_user_comment, :update_judgement, :update_expiry
       post :upload_image
       get  :edit_image
       delete :delete_image
-
-      patch :update_expiry
     end
   end
-
-  devise_scope :user do
-    get 'guest_sign_in', to: 'users/sessions#guest_sign_in'
-    get 'admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
-  end
-  #devise_scope :user do
-  #  get '/logout', to: 'devise/sessions#destroy', as: :logout, via: [:get, :post]
-  #end
-
-  # resources :documents do
-  #   post 'upload_image', on: :member
-  #   delete 'delete_image', on: :member
-  # end
 
   # 健康確認用
   get "up" => "rails/health#show", as: :rails_health_check
