2548f4256b874fd86959026e90293f030b606773
moaoyama <mo.aoyama.303@gmail.com>
2025-09-08 22:23:40 +0900

    20250908_add_and_update

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,10 @@
+# app/controllers/errors_controller.rb
 class ErrorsController < ApplicationController
-  # 404 Not Found
   def not_found
-    render status: :not_found
+    render status: 404
   end
 
-  # 500 Internal Server Error
   def internal_server_error
-    render status: :internal_server_error
+    render status: 500
   end
 end
