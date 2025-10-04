d5d4ca3ec5f26d821aa37a77d1f15fb629d7ff3f
moaoyama <mo.aoyama.303@gmail.com>
2025-10-04 22:21:10 +0900

    refactor: remove unused comments

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,36 +4,13 @@ RailsAdmin.config do |config|
   end
   config.current_user_method(&:current_user)
 
-  ## 管理者以外はアクセス拒否
+  # 管理者以外はアクセス拒否
   config.authorize_with do
     unless current_user&.admin?
       flash[:alert] = "管理者のみアクセスできます"
       redirect_to main_app.root_path
     end
   end
-
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
 
   config.actions do
     dashboard                     # mandatory
